2288305a4781ff2679b1b73af00d394ec9b8effe
mateustip <mateuslucas.ti@gmail.com>
2021-06-04 09:02:40 -0300

    Start of configuration in the application

---
+++ Netflix/Netflix/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Netflix
+//
+//  Created by Matheus Lucas on 04/06/21.
+//
+
+import SwiftUI
+import CoreData
+
+struct ContentView: View {
+    
+    var body: some View {
+        Text("Netflix")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Netflix/Netflix/NetflixApp.swift
@@ -0,0 +1,18 @@
+//
+//  NetflixApp.swift
+//  Netflix
+//
+//  Created by Matheus Lucas on 04/06/21.
+//
+
+import SwiftUI
+
+@main
+struct NetflixApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
